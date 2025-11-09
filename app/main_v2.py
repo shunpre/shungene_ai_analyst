cc42339fb938b565b69c7a3a722541b1890cb3e1
瞬コピ <happier55@gmail.com>
2025-11-09 14:00:43 +0900

    #つけた

--- app/main_v2.py
+++ app/main_v2.py
@@ -30,7 +30,7 @@ hide_decoration_bar_style = '''
         header {visibility: hidden;}
     </style>
 '''
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+#st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
 # ブラウザがスクロールする先の「基点」を設置
 st.markdown('<a id="top-anchor"></a>', unsafe_allow_html=True)
